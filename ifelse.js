f84c3bf33a32dd9c2a6c8ae57d33b2437e3cdde4
SamLinoFinnegan <73761381+SamLinoFinnegan@users.noreply.github.com>
2020-11-25 11:31:49 +0000

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,19 @@
+const age = 25
+const isFemale = false
+const driverStatus = 'bob'
+if (age >= 18) {
+    console.log("Welkome to Finnegan Spirits, we hope you enjoy your stay")
+}
+else {
+    console.log("Im sorry but you are still under 18 therefore you cannot come in")
+}
+if (isFemale) {
+    console.log("Thank you for making your reservation for the anual ladies night at the Finnegans Spirits Bar")
+} else {
+    console.log("Unfortunately this party is for ladies only, feel free to join our Gentelmens Club")
+}
+if (driverStatus == 'bob') {
+    console.log("Thank you Bob, please make sure our guets return home safely")
+} else {
+    console.log("Im sorry Sr but i cannot allow you to drive, give me a minute and ill ask Bob to take you home")
+}
